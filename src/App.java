6f7bbac7b4bb60b2414d18a9704ebd7b3e6d3a9d
Vincent Brusco <vbrusco30794@gmail.com>
2022-11-22 10:28:49 -0500

    Added insertion sort

--- src/App.java
+++ src/App.java
@@ -1,11 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int arrSize = 200000;
-        int largestNum = 1000;
+        int arrSize = 100000;
+        int largestNum = 20000;
         int[] arr = NumberGenerator.generateArray(arrSize, largestNum);
+        long start = 0;
+        long end = 0;
 
-        Sort.bubbleSort(arr);
-        System.out.println(Sort.isSorted(arr));
+        int[] bubbleArray = arr;
+        int[] insertionArray = arr;
+
+        // Sort.printArray(arr, "unsorted");
+        start = System.currentTimeMillis();
+        Sort.insertionSort(insertionArray);
+        end = System.currentTimeMillis();
+        // Sort.printArray(arr, "sorted");
+        System.out.println(Sort.isSorted(insertionArray));
+        System.out.println("Insertion sort took " + (end - start) + "ms");
+
+        // Sort.printArray(arr, "unsorted");
+        start = System.currentTimeMillis();
+        Sort.bubbleSort(bubbleArray);
+        end = System.currentTimeMillis();
+        // Sort.printArray(arr, "sorted");
+        System.out.println(Sort.isSorted(bubbleArray));
+        System.out.println("Bubble sort took " + (end - start) + "ms");
 
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -2,7 +2,6 @@ public class Sort {
     public static int[] bubbleSort(int[] arr) {
         int len = arr.length;
         int temp = 0;
-
         while (len != 0) {
             for (int i = 0; i < len - 1; i++) {
                 if (arr[i] > arr[i + 1]) {
@@ -13,6 +12,91 @@ public class Sort {
             }
             len--;
         }
+        return arr;
+    }
+
+    public static int[] insertionSort(int[] arr) {
+        int temp = 0;
+        int place = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+            if (place > 0) {
+                for (int j = place; j > 0; j--) {
+                    if (arr[j] < arr[j - 1]) {
+                        temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    } else
+                        break;
+                }
+            }
+
+            place++;
+        }
+
+        return arr;
+    }
+
+    public static int[] mergeSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] selectionSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] quickSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] heapSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] countingSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] radixSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] bucketSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] shellSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] timSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] combSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] pigeonholeSort(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] cycleSort(int[] arr) {
 
         return arr;
     }
